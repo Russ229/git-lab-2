c49aa9a461cb69c323887dc067b59577a24eea5a
Russel <aa278424@ohio.edu>
2025-09-05 16:12:34 -0400

    code stuff

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Russ Allphin>
+ *        Date: <Sep 5>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!! \n";
+        return 0;
+}// main
